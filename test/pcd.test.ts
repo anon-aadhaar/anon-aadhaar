bca57719dc66b910941ac86269dd099fbb900c60
Yanis Meziane <yanis.meziane@essec.edu>
2023-07-25 21:17:26 +0200

    commented web prover test

--- test/pcd.test.ts
+++ test/pcd.test.ts
@@ -49,39 +49,38 @@ describe('PCD tests', function () {
     assert(verified == true, 'Should verifiable')
   })
 
-  // TODO: Create utils for test Web Prover
-  it('PCD flow web prover', async function () {
-    const pcdInitArgs: PCDInitArgs = {
-      wasmURL:
-        'https://d3dxq5smiosdl4.cloudfront.net/rsa_verify_sha1_pkcs1v15.wasm',
-      zkeyURL: 'https://d3dxq5smiosdl4.cloudfront.net/circuit_final.zkey',
-      isWebEnv: true,
-    }
+  // it('PCD flow web prover', async function () {
+  //   const pcdInitArgs: PCDInitArgs = {
+  //     wasmURL:
+  //       'https://d3dxq5smiosdl4.cloudfront.net/rsa_verify_sha1_pkcs1v15.wasm',
+  //     zkeyURL: 'https://d3dxq5smiosdl4.cloudfront.net/circuit_final.zkey',
+  //     isWebEnv: true,
+  //   }
 
-    await init(pcdInitArgs)
+  //   await init(pcdInitArgs)
 
-    const pcdArgs: IdentityPCDArgs = {
-      exp: {
-        argumentType: ArgumentTypeName.BigInt,
-        value: BigInt(65337) + '',
-      },
-      signature: {
-        argumentType: ArgumentTypeName.BigInt,
-        value: testData[1] + '',
-      },
-      mod: {
-        argumentType: ArgumentTypeName.BigInt,
-        value: testData[2] + '',
-      },
-      message: {
-        argumentType: ArgumentTypeName.BigInt,
-        value: testData[3] + '',
-      },
-    }
+  //   const pcdArgs: IdentityPCDArgs = {
+  //     exp: {
+  //       argumentType: ArgumentTypeName.BigInt,
+  //       value: BigInt(65337) + '',
+  //     },
+  //     signature: {
+  //       argumentType: ArgumentTypeName.BigInt,
+  //       value: testData[1] + '',
+  //     },
+  //     mod: {
+  //       argumentType: ArgumentTypeName.BigInt,
+  //       value: testData[2] + '',
+  //     },
+  //     message: {
+  //       argumentType: ArgumentTypeName.BigInt,
+  //       value: testData[3] + '',
+  //     },
+  //   }
 
-    const pcd = await prove(pcdArgs)
+  //   const pcd = await prove(pcdArgs)
 
-    const verified = await verify(pcd)
-    assert(verified == true, 'Should verifiable')
-  })
+  //   const verified = await verify(pcd)
+  //   assert(verified == true, 'Should verifiable')
+  // })
 })
