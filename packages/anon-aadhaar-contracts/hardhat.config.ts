0dad71a49a7b61f83895a5b32d703182d66791a8
Yanis Meziane <yanis.meziane@essec.edu>
2023-11-22 17:42:45 +0100

    fix hardhat test error

--- packages/anon-aadhaar-contracts/hardhat.config.ts
+++ packages/anon-aadhaar-contracts/hardhat.config.ts
@@ -5,16 +5,16 @@ require('dotenv').config()
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
-  networks: {
-    goerli: {
-      url: 'https://ethereum-goerli.publicnode.com',
-      accounts: [process.env.PRIVATE_KEY || ''],
-    },
-    mumbai: {
-      url: 'https://rpc-mumbai.maticvigil.com',
-      accounts: [process.env.PRIVATE_KEY || ''],
-    },
-  },
+  // networks: {
+  //   goerli: {
+  //     url: 'https://ethereum-goerli.publicnode.com',
+  //     accounts: [process.env.PRIVATE_KEY || ''],
+  //   },
+  //   mumbai: {
+  //     url: 'https://rpc-mumbai.maticvigil.com',
+  //     accounts: [process.env.PRIVATE_KEY || ''],
+  //   },
+  // },
 }
 
 export default config
