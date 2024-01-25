2ba7e48c591bcd3f73369c2577f542c75c090809
Meyanis95 <yanis.meziane@essec.edu>
2024-01-25 18:23:26 +0100

    still wip

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -191,7 +191,8 @@ export function decompressByteArray(byteArray: Uint8Array) {
 }
 
 export const enum SELECTOR_ID {
-  emailOrPhone = 0,
+  null = 0,
+  emailOrPhone,
   referenceId,
   name,
   dob,
@@ -209,82 +210,23 @@ export const enum SELECTOR_ID {
   VTC,
 }
 
-export class SelectorBuilder {
-  selector: number[]
-
-  constructor() {
-    this.selector = new Array(16).fill(0)
-  }
-
-  selectEmailOrPhone() {
-    this.selector[0] = 1
-    return this
-  }
-
-  selectReferenceId() {
-    this.selector[1] = 1
-    return this
-  }
-  selectName() {
-    this.selector[2] = 1
-    return this
-  }
-  selectDoB() {
-    this.selector[3] = 1
-    return this
-  }
-  selectGender() {
-    this.selector[4] = 1
-    return this
-  }
-  selectCareof() {
-    this.selector[5] = 1
-    return this
-  }
-  selectDistrict() {
-    this.selector[6] = 1
-    return this
-  }
-  selectLandmark() {
-    this.selector[7] = 1
-    return this
-  }
-  selectHouse() {
-    this.selector[8] = 1
-    return this
-  }
-  selectLocation() {
-    this.selector[9] = 1
-    return this
-  }
-  selectPinCode() {
-    this.selector[10] = 1
-    return this
-  }
-  selectPostOffice() {
-    this.selector[11] = 1
-    return this
-  }
-  selectState() {
-    this.selector[12] = 1
-    return this
-  }
-  selectStreet() {
-    this.selector[13] = 1
-    return this
-  }
-  selectSubDistrict() {
-    this.selector[14] = 1
-    return this
-  }
-  selectVTC() {
-    this.selector[15] = 1
-    return this
-  }
-
-  build() {
-    return this.selector
-  }
+export const enum SELECTOR_TEST_ID {
+  emailOrPhone = 0,
+  referenceId,
+  name,
+  dob,
+  gender,
+  careOf,
+  district,
+  landmark,
+  house,
+  location,
+  pinCode,
+  postOffice,
+  state,
+  street,
+  subDistrict,
+  VTC,
 }
 
 export function readData(data: number[], index: number) {
