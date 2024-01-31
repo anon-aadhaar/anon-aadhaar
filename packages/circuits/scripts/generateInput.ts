5588d8fb7af92b12892554008f42e70188d5466a
Saleel <saleel@saleel.xyz>
2024-01-31 12:16:10 +0530

    update generate input script to use QR data

--- packages/circuits/scripts/generateInput.ts
+++ packages/circuits/scripts/generateInput.ts
@@ -1,27 +1,59 @@
-import { writeFileSync } from 'fs'
-import { hash, splitToWords } from '@anon-aadhaar/core'
-import { genData } from '../../core/test/utils'
+import { readFileSync, writeFileSync } from 'fs'
+import crypto from 'crypto'
+import path from 'path'
+import {
+  hash,
+  splitToWords,
+  convertBigIntToByteArray,
+  decompressByteArray,
+} from '@anon-aadhaar/core'
+import {
+  Uint8ArrayToCharArray,
+  bufferToHex,
+} from '@zk-email/helpers/dist/binaryFormat'
 import { sha256Pad } from '@zk-email/helpers/dist/shaHash'
-import { Uint8ArrayToCharArray } from '@zk-email/helpers/dist/binaryFormat'
 
 const main = () => {
-  const signedData = 'Hello-20240116140412'
+  // We are using produciton public key here (v2)
+  // Change to uidai_prod_cdup.cer to use the test data provided by UIDAI (v1)
+  const pkData = readFileSync(
+    path.join(__dirname, '../assets', 'uidai_offline_publickey_26022021.cer'),
+  )
+  const pk = crypto.createPublicKey(pkData)
 
-  genData(signedData, 'SHA-256').then(data => {
-    const [paddedMsg, messageLen] = sha256Pad(
-      Buffer.from(signedData, 'ascii'),
-      512 * 3,
-    )
+  // Add QR data here (bigInt)
+  const QRData = BigInt('')
 
-    const input = {
-      aadhaarData: Uint8ArrayToCharArray(paddedMsg),
-      aadhaarDataLength: messageLen,
-      signature: splitToWords(data[1], BigInt(64), BigInt(32)),
-      pubKey: splitToWords(data[2], BigInt(64), BigInt(32)),
-      signalHash: hash(1),
-    }
-    writeFileSync('build/input.json', JSON.stringify(input))
-  })
+  const qrDataBytes = convertBigIntToByteArray(BigInt(QRData))
+  const decodedData = decompressByteArray(qrDataBytes)
+
+  const signatureBytes = decodedData.slice(
+    decodedData.length - 256,
+    decodedData.length,
+  )
+
+  const signedData = decodedData.slice(0, decodedData.length - 256)
+  const [paddedMsg, messageLen] = sha256Pad(signedData, 512 * 3)
+
+  const pubKey = BigInt(
+    '0x' +
+      bufferToHex(
+        Buffer.from(pk.export({ format: 'jwk' }).n as string, 'base64url'),
+      ),
+  )
+
+  const signature = BigInt(
+    '0x' + bufferToHex(Buffer.from(signatureBytes)).toString(),
+  )
+
+  const input = {
+    aadhaarData: Uint8ArrayToCharArray(paddedMsg),
+    aadhaarDataLength: messageLen,
+    signature: splitToWords(signature, BigInt(64), BigInt(32)),
+    pubKey: splitToWords(pubKey, BigInt(64), BigInt(32)),
+    signalHash: hash(1),
+  }
+  writeFileSync('build/input.json', JSON.stringify(input))
 }
 
 main()
