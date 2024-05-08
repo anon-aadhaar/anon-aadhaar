af8405af9a85869946869c7d70642732d243067a
Meyanis95 <yanis.meziane@essec.edu>
2024-05-08 20:31:20 +0900

    feat: uploaded new keys

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,13 +1,13 @@
 enum BaseUrlArtifacts {
   Prod = 'https://d1re67zv2jtrxt.cloudfront.net/',
   // Test = 'https://d1l6t78iyuhldt.cloudfront.net/v1.0.0',
-  Staging = 'https://dhzposh38rt8g.cloudfront.net',
+  Staging = 'https://anon-aadhaar-staging.s3.eu-west-3.amazonaws.com/',
 }
 
 enum Version {
   V1 = 'v1.0.0',
   V2 = 'v2.0.0',
-  Lite = 'lite',
+  Lite = 'Lite',
 }
 
 enum Artifacts {
@@ -15,6 +15,9 @@ enum Artifacts {
   ZKEY_URL = '/circuit_final.zkey',
   VK_URL = '/vkey.json',
   ZKEY_CHUNKS = '/chunked_zkey',
+  WASM_URL_LITE = '/aadhaar-verifier.afk.wasm',
+  ZKEY_URL_LITE = '/aadhaar-verifier.afk.zkey',
+  VK_URL_LITE = '/aadhaar-verifier.afk.vkey.json',
 }
 
 export const artifactUrls = {
@@ -31,10 +34,10 @@ export const artifactUrls = {
     chunked: BaseUrlArtifacts.Prod + Version.V2 + Artifacts.ZKEY_CHUNKS,
   },
   Lite: {
-    wasm: BaseUrlArtifacts.Prod + Version.Lite + Artifacts.WASM_URL,
-    zkey: BaseUrlArtifacts.Prod + Version.Lite + Artifacts.ZKEY_URL,
-    vk: BaseUrlArtifacts.Prod + Version.Lite + Artifacts.VK_URL,
-    chunked: BaseUrlArtifacts.Prod + Version.Lite + Artifacts.ZKEY_CHUNKS,
+    wasm: BaseUrlArtifacts.Staging + Version.Lite + Artifacts.WASM_URL_LITE,
+    zkey: BaseUrlArtifacts.Staging + Version.Lite + Artifacts.ZKEY_URL_LITE,
+    vk: BaseUrlArtifacts.Staging + Version.Lite + Artifacts.VK_URL_LITE,
+    chunked: BaseUrlArtifacts.Staging + Version.Lite + Artifacts.ZKEY_CHUNKS,
   },
   // test: {
   //   wasm: BaseUrlArtifacts.Test + Artifacts.WASM_URL,
