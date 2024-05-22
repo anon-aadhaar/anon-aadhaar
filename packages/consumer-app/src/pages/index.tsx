1b5d344c3d31fe7b2a1062784f0bc2bf37eb8297
Meyanis95 <yanis.meziane@essec.edu>
2024-05-22 20:50:13 +0900

    added download button

--- packages/consumer-app/src/pages/index.tsx
+++ packages/consumer-app/src/pages/index.tsx
@@ -104,6 +104,19 @@ export default function Home() {
                 <Badge label={bigIntsToString([BigInt(user.claimValues_5)])} />
               )}
             </div>
+
+            <div>
+              <button
+                type="button"
+                className="rounded-md bg-white px-3 py-2 mt-10 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-red-200 hover:bg-gray-50"
+                onClick={() => {
+                  router.push('/')
+                  setUser(null)
+                }}
+              >
+                Logout
+              </button>
+            </div>
           </div>
         ) : (
           <>
