bf3b4f5a2d4e9bc66ba34549584e392a145edcef
Saleel <saleel@saleel.xyz>
2024-07-24 20:12:12 +0400

    core: update nullifier type

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -44,3 +44,8 @@ export const artifactUrls = {
 
 export const testCertificateUrl =
   'https://anon-aadhaar.s3.ap-south-1.amazonaws.com/testCertificate.pem'
+
+
+export const CIRCOM_FIELD_P = BigInt(
+  '21888242871839275222246405745257275088548364400416034343698204186575808495617'
+);
